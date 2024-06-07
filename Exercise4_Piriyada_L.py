5acb8d62f005c835cd44aceb3f0be15e57e5f1a7
Momizzz <134781186+Momizzz@users.noreply.github.com>
2024-06-07 13:34:14 +0700

    Create Exercise4_Piriyada_L.py

---
+++ Exercise4_Piriyada_L.py
@@ -0,0 +1,10 @@
+fe = float(input()) #Foundation English
+gb = float(input()) #General Business
+ics = float(input()) #Introduction to Computer Systems
+cp = float(input()) #Computer Programming
+
+print("--- Your score ---")
+print("Foundation English : ", fe)
+print("General Business : ", gb)
+print("Introducrion to Computer System : ", ics)
+print("Computer Programming :", cp)
