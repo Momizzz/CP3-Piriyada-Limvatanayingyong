a2f9af28c0508c19b75e55db98994be362ba0fe1
Momizzz <134781186+Momizzz@users.noreply.github.com>
2024-06-12 18:52:13 +0700

    Update Exercise8_Piriyada_L.py

--- Exercise8_Piriyada_L.py
+++ Exercise8_Piriyada_L.py
@@ -1,29 +1,33 @@
 usernameInput = input("Username : ")
 passwordInput = input("Password : ")
 
-if usernameInput == "Piriyada" and passwordInput == "13579" :
+if usernameInput == "Hungry" and passwordInput == "5555" :
   print("Welcome", usernameInput)
-  print("------Hunger Shop------")
+  print("-------------Hunger Shop-------------")
   print("These are our Menus :")
-  print("Mineral Water  |  20  THB")
-  print("Soft Drink     |  25  THB")
-  print("Potato Chips   |  50  THB")
-  print("Candy Bar      |  15  THB")
-  print("Popsicle       |  40  THB")
+  print("Mineral Water       |  20  THB")
+  print("Soft Drink          |  25  THB")
+  print("Potato Chips        |  50  THB")
+  print("Candy Bar           |  15  THB")
+  print("Popsicle            |  40  THB")
   order = input("Please choose your order: ")
-  if order1 == "Mineral Water":
-    price1 = int(input("Amount : "))
+  if order == "Mineral Water":
+    price1 = int(input("Amount      : "))
+    print("Total       :", price1*20, "THB")
   elif order == "Soft Drink":
-    price2 = int(input("Amount : "))
+    price2 = int(input("Amount      : "))
+    print("Total       :", price2*25, "THB")
   elif order == "Potato Chips":
-    price3 = int(input("Amount : "))
+    price3 = int(input("Amount      : "))
+    print("Total       :", price3*50, "THB")
   elif order == "Candy Bar":
-    price4 = int(input("Amount : "))
+    price4 = int(input("Amount      : "))
+    print("Total       :", price4*15, "THB")
   elif order == "Popsicle":
-    price5 = int(input("Amount : "))
-  print("Total        :", 20*price1 + 25*price2 + 50*price3 + 15*price4 + 40*price5)
+    price5 = int(input("Amount      : "))
+    print("Total       :", price5*40, "THB")
   print("Thank You And Enjoy!")
-  print("---------------------------------------")
+  print("--------------------------------------")
 else:
   print("Log-in Fail")
   print("Please try again !")
